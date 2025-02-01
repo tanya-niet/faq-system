89c5d27f178e6b5a32068d314f39caed8d8db65a
tanya_31 <tanya.nsit20@gmail.com>
2025-02-02 01:29:45 +0530

    Remove all files from the repository

--- faq_system/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FaqSystemConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'faq_system'
--- faq_system/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import FAQ
-
-class FAQSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FAQ
-        fields = ['id', 'question', 'answer', 'question_hi', 'question_bn']
